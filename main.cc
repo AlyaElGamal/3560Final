cd52fded4f9a066095d1f9b4da74d3f45eff0055
Alya ElGamal <ae013715@ohio.edu>
2017-12-12 17:55:30 -0500

    updated with doxygen documentation

--- main.cc
+++ main.cc
@@ -1,4 +1,46 @@
 /**********
 
-Main.cc
+@title: Main.cc
+@brief: title of function
+@author: Alya ElGamal
 *********/
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include "countLine.h"
+#include "countChar.h"
+
+/**
+The main function of the program
+@author: Alya ElGamal
+@param: none
+*/
+int main(){
+	string temp = "Ohio University";
+	string temp2 = "Athens";
+	ostream* outs;
+
+}
+
+/*
+Th function that counts the characters in the file
+@author: Alya ElGamal
+@param: istream ins
+*/
+int countChar(istream* ins){
+	int counter=0;
+	string tmp;
+	char c[200];
+	while(!file.eof()){
+		tmp.getline(c);
+	}
+}
+
+/*
+The function that counts the number of lines in a file
+@author: Alya ElGamal
+@param: istream ins
+*/
+int countLines(istream* ins){
+
+}
